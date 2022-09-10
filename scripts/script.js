600af73bd0371fa5c555cd7645948c986cd8fa49
Kirill Melnikov <kvmelnikov@gmail.com>
2022-09-10 18:45:40 +0300

    add add_card

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,78 @@
 // first part Popup
-
+//popup
 const popup = document.querySelector('#popup');
 const closeButton = popup.querySelector('#close_modal');
 const editButton = document.querySelector('#profile__edit-button');
-const nameInput = document.querySelector('#name__input');
-const descriptionInput = document.querySelector('#description__input');
+//form
+const firstInput = document.querySelector('#first__input');
+const secondInput = document.querySelector('#second__input');
+const formTitle = document.querySelector('#form__title');
 const formButton = document.querySelector('#form__button');
+const form = document.querySelector('#form');
+//profile
 const profileHeader = document.querySelector('#profile__name-header');
 const profileDescript = document.querySelector('#profile__descript');
+const addButton = document.querySelector('#profile__add-button');
 
 
-
-function EditForm() {
-  nameInput.value = profileHeader.textContent.trim();
-  descriptionInput.value = profileDescript.textContent.trim();
+function FormValue(type) {
+  if(type === 'edit-form') {
+    console.log(type)
+    form.name = type
+    firstInput.value = profileHeader.textContent.trim();
+    secondInput.value = profileDescript.textContent.trim();
+    
+  }
+  else if (type === 'add-form') {
+    firstInput.value  = "";
+    secondInput.value = "";
+    firstInput.attributes.placeholder.value = "Название";
+    secondInput.attributes.placeholder.value = "Ссылка на картинку";
+    form.name = type
+  }
 }
 
-function EditProfile() {
-  profileHeader.textContent = nameInput.value;
-  profileDescript.textContent = descriptionInput.value;
+function SubmitForm() {
+  console.log(' run submit', form)
+  if(form.attributes.name.value === 'edit-form') {
+    profileHeader.textContent = firstInput.value;
+    profileDescript.textContent = secondInput.value;
+  }
+  else if(form.attributes.name.value === 'add-form')  {
+    card = {name: firstInput.value, link: secondInput.value}
+    Cards.append(AddOneCard(card));
+  }
 } 
 
 function Popup(){
-  EditForm();
   popup.classList.toggle('popup_opened');
 }
 
 formButton.addEventListener('click', (event) => {
   event.preventDefault();
-  EditProfile();
+  SubmitForm();
   Popup();
-})
+});
+
 
 closeButton.addEventListener('click', Popup);
-editButton.addEventListener('click', Popup);
+editButton.addEventListener('click', () => {
+  FormValue('edit-form');
+  Popup();
+});
+
+addButton.addEventListener('click', () => {
+  FormValue('add-form');
+  Popup();
+});
+
+
+// formButton.addEventListener('click', () => {
+//   card = {name: firstInput.value, link: seconInput.value}
+//   Cards.append(AddOneCard(card));
+
+// }); 
+
 
 //two part Cards
 
@@ -81,30 +120,39 @@ function createElementCard()  {
   return { cardArticle:  cardArticle, cardImage: cardImage, cardName: cardName, cardText: cardText, cardHeart: cardHeart }
 }
 
+function AddOneCard(card) {
+  element = createElementCard();
+  element.cardText.textContent = card.name;
+  element.cardName.append(element.cardText);
+  element.cardName.append(element.cardHeart);
+  element.cardImage.src = card.link;
+  element.cardImage.alt = card.name;
+  element.cardArticle.append(element.cardImage, element.cardName);
+  return element.cardArticle;
+}
 
-function AddCard (cards = null) {
+
+
+function AddCards (cards = null) {
   const card_for = [];
   if (cards) {
     for(let i = 0; i < cards.length; i++) {
-      element = createElementCard();
-      element.cardText.textContent = cards[i].name;
-      element.cardName.append(element.cardText);
-      element.cardName.append(element.cardHeart);
-      element.cardImage.src = cards[i].link;
-      element.cardImage.alt = cards[i].name;
-      element.cardArticle.append(element.cardImage, element.cardName);
-      Cards.append(element.cardArticle);
+      article = AddOneCard(cards[i])
+      Cards.append(article);
     }
   }
   else {
       console.log('not cards');
   }
-  
-
 }
 
+AddCards(initialCards);
 
-AddCard(initialCards);
+// three part add Card form
+
+
+
+
 
 
 
