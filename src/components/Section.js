b5a72b4e5bc6d110ed337123fd2baa86957a4e40
vladimirgromozdin <70908406+vladimirgromozdin@users.noreply.github.com>
2023-02-04 17:36:12 +0400

    temp: add Section progress before pull

---
+++ src/components/Section.js
@@ -0,0 +1,13 @@
+export default class Section {
+    constructor({ items, renderer }, selector) {
+        this._items = items;
+        this._renderer = renderer;
+    }
+
+    render() {
+
+    }
+
+    addItem() {}
+
+}
